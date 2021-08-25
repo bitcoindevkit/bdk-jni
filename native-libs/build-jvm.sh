8d092f0abb867682f43867f95163921fc9231331
Steve Myers <steve@notmandatory.org>
2021-08-25 14:47:26 +0200

    Build jvm native libs for linux or darwin (osx)

--- native-libs/build-jvm.sh
+++ native-libs/build-jvm.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail -o xtrace
 
-# IMPORTANT: make sure every target is not a substring of a different one. We check for them with grep later on
-BUILD_TARGETS="${BUILD_TARGETS:-linux}"
+BUILD_TARGET=$(uname | tr "[:upper:]" "[:lower:]")
 
-mkdir -p ../jvm/build/jniLibs/ ../jvm/build/jniLibs/x86_64_linux/
+mkdir -p ../jvm/build/jniLibs/
 
-if echo $BUILD_TARGETS | grep "linux"; then
+if echo $BUILD_TARGET | grep "linux"; then
     cargo build --target=x86_64-unknown-linux-gnu
-    cp target/x86_64-unknown-linux-gnu/debug/libbdk_jni.so ../jvm/build/jniLibs/x86_64_linux/
+    cp target/x86_64-unknown-linux-gnu/debug/libbdk_jni.so ../jvm/build/jniLibs/
+elif echo $BUILD_TARGET | grep "darwin"; then
+    cargo build --target=x86_64-apple-darwin
+    cp target/x86_64-apple-darwin/debug/libbdk_jni.dylib ../jvm/build/jniLibs/
+else
+    echo "Unknown jvm target $BUILD_TARGET"
+    exit 1
 fi
