f96c36e89358066e48a8e7c037f3f359ef486f32
Steve Myers <steve@notmandatory.org>
2021-08-25 16:54:18 +0200

    Build native-libs in release mode

--- native-libs/build-android.sh
+++ native-libs/build-android.sh
@@ -16,18 +16,18 @@ BUILD_TARGETS="${BUILD_TARGETS:-aarch64,armv7,x86_64,i686}"
 mkdir -p ../android/src/main/jniLibs/ ../android/src/main/jniLibs/arm64-v8a ../android/src/main/jniLibs/x86_64 ../android/src/main/jniLibs/armeabi-v7a ../android/src/main/jniLibs/x86
 
 if echo $BUILD_TARGETS | grep "aarch64"; then
-    CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="aarch64-linux-android21-clang" CC="aarch64-linux-android21-clang" cargo build --target=aarch64-linux-android
-    cp target/aarch64-linux-android/debug/libbdk_jni.so ../android/src/main/jniLibs/arm64-v8a
+    CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="aarch64-linux-android21-clang" CC="aarch64-linux-android21-clang" cargo build --release --target=aarch64-linux-android
+    cp target/aarch64-linux-android/release/libbdk_jni.so ../android/src/main/jniLibs/arm64-v8a
 fi
 if echo $BUILD_TARGETS | grep "x86_64"; then
-    CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="x86_64-linux-android21-clang" CC="x86_64-linux-android21-clang" cargo build --target=x86_64-linux-android
-    cp target/x86_64-linux-android/debug/libbdk_jni.so ../android/src/main/jniLibs/x86_64
+    CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="x86_64-linux-android21-clang" CC="x86_64-linux-android21-clang" cargo build --release --target=x86_64-linux-android
+    cp target/x86_64-linux-android/release/libbdk_jni.so ../android/src/main/jniLibs/x86_64
 fi
 if echo $BUILD_TARGETS | grep "armv7"; then
-    CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="armv7a-linux-androideabi21-clang" CC="armv7a-linux-androideabi21-clang" cargo build --target=armv7-linux-androideabi
-    cp target/armv7-linux-androideabi/debug/libbdk_jni.so ../android/src/main/jniLibs/armeabi-v7a
+    CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="armv7a-linux-androideabi21-clang" CC="armv7a-linux-androideabi21-clang" cargo build --release --target=armv7-linux-androideabi
+    cp target/armv7-linux-androideabi/release/libbdk_jni.so ../android/src/main/jniLibs/armeabi-v7a
 fi
 if echo $BUILD_TARGETS | grep "i686"; then
-    CARGO_TARGET_I686_LINUX_ANDROID_LINKER="i686-linux-android21-clang" CC="i686-linux-android21-clang" cargo build --target=i686-linux-android
-    cp target/i686-linux-android/debug/libbdk_jni.so ../android/src/main/jniLibs/x86
+    CARGO_TARGET_I686_LINUX_ANDROID_LINKER="i686-linux-android21-clang" CC="i686-linux-android21-clang" cargo build --release --target=i686-linux-android
+    cp target/i686-linux-android/release/libbdk_jni.so ../android/src/main/jniLibs/x86
 fi
--- native-libs/build-jvm.sh
+++ native-libs/build-jvm.sh
@@ -6,11 +6,11 @@ BUILD_TARGET=$(uname | tr "[:upper:]" "[:lower:]")
 mkdir -p ../jvm/build/jniLibs/
 
 if echo $BUILD_TARGET | grep "linux"; then
-    cargo build --target=x86_64-unknown-linux-gnu
-    cp target/x86_64-unknown-linux-gnu/debug/libbdk_jni.so ../jvm/build/jniLibs/
+    cargo build --release --target=x86_64-unknown-linux-gnu
+    cp target/x86_64-unknown-linux-gnu/release/libbdk_jni.so ../jvm/build/jniLibs/
 elif echo $BUILD_TARGET | grep "darwin"; then
-    cargo build --target=x86_64-apple-darwin
-    cp target/x86_64-apple-darwin/debug/libbdk_jni.dylib ../jvm/build/jniLibs/
+    cargo build --release --target=x86_64-apple-darwin
+    cp target/x86_64-apple-darwin/release/libbdk_jni.dylib ../jvm/build/jniLibs/
 else
     echo "Unknown jvm target $BUILD_TARGET"
     exit 1
